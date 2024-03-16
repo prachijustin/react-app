eb2931fa6792b16820101051d1d663bfd72172b5
PrachiS <prachi.sharma@cytel.com>
2024-03-16 14:38:51 +0530

    first commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,18 @@
+function ListGroup() {
+  const items = ["India", "USA", "Canada", "Germany", "Australia"];
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <li
+          className="list-group-item"
+          key={item}
+          onClick={() => console.log(item)}
+        >
+          {item}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default ListGroup;
